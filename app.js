d69a09ae3e4e9ab8908da104e5cb73c13b694cb9
asantesylvesterkwame <intelligentson17@gmail.com>
2023-01-22 16:09:34 +0000

    created an option fuctionality

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
-let game = {
-    gameBoard: []
+// let game = {
+//     gameBoard: []
+// }
+
+
+let gameLevel = document.querySelector('#gameLevel');
+
+function logValue() {
+    console.log(gameLevel.value);
 }
+
+let xPlayerButton = document.getElementById('XplayerBtn');
+
+let oPlayerButton = document.getElementById('OplayerButton');
+
+let gameBoardInput = document.querySelectorAll('.gameBoardInput');
+xPlayerButton.addEventListener('click', function () {
+    let xButtonName = xPlayerButton.name = 'Xfocused';
+    console.log(xButtonName);
+    let oButtonName = oPlayerButton.name = '';
+    console.log(oButtonName);
+    if (gameLevel.value === 'easy') {
+        if (xButtonName === 'Xfocused') {
+            gameBoardInput.addEventListener('click', function () {
+                gameBoardInput.innerHTML = 'X'
+            })
+        }
+    }
+});
+
+oPlayerButton.addEventListener('focus', function () {
+    let xButtonName = xPlayerButton.name = '';
+    console.log(xButtonName);
+    let oButtonName = oPlayerButton.name = 'Ofocused';
+    console.log(oButtonName);
+    
+});
 
 
